3f9723aac38aaa8acc89719316968a0f8bcc165d
Dogan Ucar <dogan@dogan-ucar.de>
2018-10-21 17:06:20 +0200

    new utility function for numbers

--- src/Util/NumberUtil.php
+++ src/Util/NumberUtil.php
@@ -25,80 +25,110 @@ namespace doganoo\PHPUtil\Util;
  *
  * @package doganoo\PHPUtil\Util
  */
-final class NumberUtil{
-	/**
-	 * prevent from instantiation
-	 * StringUtil constructor.
-	 */
-	private function __construct(){
-	}
+final class NumberUtil {
+    /**
+     * prevent from instantiation
+     * StringUtil constructor.
+     */
+    private function __construct() {
+    }
 
-	/**
-	 * returns the formatted number with grouped thousands
-	 *
-	 * @param     $value
-	 * @param int $decimals
-	 *
-	 * @return null|string
-	 */
-	public static function format($value, int $decimals = 2): ?string{
-		if(!\is_numeric($value)){
-			return null;
-		}
-		return \number_format($value, $decimals);
-	}
+    /**
+     * returns the formatted number with grouped thousands
+     *
+     * @param     $value
+     * @param int $decimals
+     *
+     * @return null|string
+     */
+    public static function format($value, int $decimals = 2): ?string {
+        if (!\is_numeric($value)) {
+            return null;
+        }
+        return \number_format($value, $decimals);
+    }
 
-	/**
-	 * returns an array with the elements
-	 *
-	 * @param int $number
-	 *
-	 * @return array
-	 */
-	public static function intToArray(int $number): array{
-		return StringUtil::stringToArray($number);
-	}
+    /**
+     * returns an array with the elements
+     *
+     * @param int $number
+     *
+     * @return array
+     */
+    public static function intToArray(int $number): array {
+        return StringUtil::stringToArray($number);
+    }
 
-	/**
-	 * This method checks if $value is greater than $value1. If $gte is set to
-	 * true, the method checks if $value is greater than or equal to $value1.
-	 * From http://php.net/manual/de/language.types.float.php:
-	 * So never trust floating number results to the last digit, and do not
-	 * compare floating point numbers directly for equality.
-	 * Contributed notes in http://php.net/manual/de/language.types.float.php
-	 * suggests rounding the values before comparing (see 115 catalin dot luntraru at gmail dot com).
-	 *
-	 * @param float $value
-	 * @param float $value1
-	 * @param bool  $gte
-	 *
-	 * @return bool
-	 * @since 1.0.0
-	 */
-	public static function floatGreaterThan(float $value, float $value1, bool $gte = false){
-		$value = round($value, 10, PHP_ROUND_HALF_EVEN);
-		$value1 = round($value1, 10, PHP_ROUND_HALF_EVEN);
-		if($gte){
-			return $value >= $value1;
-		} else{
-			return $value > $value1;
-		}
-	}
+    /**
+     * This method checks if $value is greater than $value1. If $gte is set to
+     * true, the method checks if $value is greater than or equal to $value1.
+     * From http://php.net/manual/de/language.types.float.php:
+     * So never trust floating number results to the last digit, and do not
+     * compare floating point numbers directly for equality.
+     * Contributed notes in http://php.net/manual/de/language.types.float.php
+     * suggests rounding the values before comparing (see 115 catalin dot luntraru at gmail dot com).
+     *
+     * @param float $value
+     * @param float $value1
+     * @param bool $gte
+     *
+     * @return bool
+     * @since 1.0.0
+     */
+    public static function floatGreaterThan(float $value, float $value1, bool $gte = false) {
+        $value = round($value, 10, PHP_ROUND_HALF_EVEN);
+        $value1 = round($value1, 10, PHP_ROUND_HALF_EVEN);
+        if ($gte) {
+            return $value >= $value1;
+        } else {
+            return $value > $value1;
+        }
+    }
 
-	/**
-	 * This method compares two float numbers for equality. PHP float values
-	 * should never be compared directly, according to: http://php.net/manual/de/language.types.float.php
-	 *
-	 * @param float $value
-	 * @param float $value1
-	 *
-	 * @return bool
-	 */
-	public static function compareFloat(float $value, float $value1){
-		$epsilon = 0.00001;
-		if(abs($value - $value1) < $epsilon){
-			return true;
-		}
-		return false;
-	}
+    /**
+     * This method compares two float numbers for equality. PHP float values
+     * should never be compared directly, according to: http://php.net/manual/de/language.types.float.php
+     *
+     * @param float $value
+     * @param float $value1
+     *
+     * @return bool
+     */
+    public static function compareFloat(float $value, float $value1) {
+        $epsilon = 0.00001;
+        if (abs($value - $value1) < $epsilon) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * strips all non integer fields from the input and returns all
+     * values as integer data type.
+     *
+     * @param array $array
+     * @return array
+     */
+    public static function stripNonInteger(array $array): array {
+        $newArray = [];
+        foreach ($array as $value) {
+            if (!NumberUtil::isInteger($value)) continue;
+            $newArray[] = \intval($value);
+        }
+        return $newArray;
+    }
+
+    /**
+     * whether $value is an integer or not.
+     * Notice that intval() returns a zero if the value is not an integer :/
+     *
+     * Therefore, we use "filter_var()" method, see here:
+     * https://stackoverflow.com/a/29018655/1966490
+     *
+     * @param $value
+     * @return bool
+     */
+    public static function isInteger($value): bool {
+        return false !== filter_var($value, FILTER_VALIDATE_INT);
+    }
 }
