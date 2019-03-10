6d20593206972adc3316589447be7743efddd7d3
Dogan Ucar <dogan@dogan-ucar.de>
2019-03-10 14:04:45 +0100

    UserService

---
+++ src/Service/User.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace doganoo\PHPUtil\Service;
+
+
+class User {
+
+    public const PASSWORD_VALIDATION_UNSAFE = 1;
+    public const PASSWORD_VALIDATION_BASIC = 2;
+    public const PASSWORD_VALIDATION_SECURE = 3;
+
+    public function validEmail(string $email): bool {
+        return \filter_var($email, \FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public function validatePassword(string $password, int $mode = self::PASSWORD_VALIDATION_SECURE): bool {
+
+        if ($mode === User::PASSWORD_VALIDATION_UNSAFE) {
+            return $this->validatePasswordUnsafe($password);
+        } else if ($mode === User::PASSWORD_VALIDATION_BASIC) {
+            return $this->validatePasswordBasic($password);
+        } else {
+            return $this->validatePasswordSecure($password);
+        }
+    }
+
+    private function validatePasswordUnsafe(string $password): bool {
+        return \strlen($password) >= 8;
+    }
+
+    private function validatePasswordBasic(string $password): bool {
+        $valid = $this->validatePasswordUnsafe($password);
+        $valid = $valid && ((bool)preg_match("(^(?=.*[a-z])(?=.*[A-Z]).+$)", $password));
+        return $valid;
+    }
+
+    private function validatePasswordSecure(string $password): bool {
+        $valid = $this->validatePasswordUnsafe($password);
+        return $valid && ((bool)preg_match("(^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$)", $password));
+    }
+}
---
+++ test/Service/User.php
@@ -0,0 +1,25 @@
+<?php
+
+
+use doganoo\PHPUtil\Service\User as UserService;
+use PHPUnit\Framework\TestCase;
+
+class User extends TestCase {
+
+    public function testValidMail() {
+        $userService = new UserService();
+        $this->assertTrue(true === $userService->validEmail("test@test.de"));
+        $this->assertTrue(false === $userService->validEmail("test@test"));
+        $this->assertTrue(false === $userService->validEmail("phputil"));
+    }
+
+    public function testValidPassword() {
+        $userService = new UserService();
+        $this->assertTrue(true === $userService->validatePassword("doganoophputil", UserService::PASSWORD_VALIDATION_UNSAFE));
+        $this->assertTrue(false === $userService->validatePassword("phputil", UserService::PASSWORD_VALIDATION_UNSAFE));
+        $this->assertTrue(true === $userService->validatePassword("Doganoophputil", UserService::PASSWORD_VALIDATION_BASIC));
+        $this->assertTrue(false === $userService->validatePassword("phputil", UserService::PASSWORD_VALIDATION_BASIC));
+        $this->assertTrue(true === $userService->validatePassword("Doganoophputil2", UserService::PASSWORD_VALIDATION_SECURE));
+        $this->assertTrue(false === $userService->validatePassword("phputil", UserService::PASSWORD_VALIDATION_SECURE));
+    }
+}
