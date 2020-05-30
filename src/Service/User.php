ca23ccc874d9e062e7ebf746237c666680e4c1cf
Dogan Ucar <dogan@dogan-ucar.de>
2020-05-30 15:08:11 +0200

    add DateTimeService

--- src/Service/User.php
+++ src/Service/User.php
@@ -1,8 +1,33 @@
 <?php
-
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Dogan Ucar, <dogan@dogan-ucar.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace doganoo\PHPUtil\Service;
 
+use function filter_var;
+use function strlen;
+use const FILTER_VALIDATE_EMAIL;
 
 class User {
 
@@ -11,7 +36,7 @@ class User {
     public const PASSWORD_VALIDATION_SECURE = 3;
 
     public function validEmail(string $email): bool {
-        return \filter_var($email, \FILTER_VALIDATE_EMAIL) !== false;
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
 
     public function validatePassword(string $password, int $mode = self::PASSWORD_VALIDATION_SECURE): bool {
@@ -26,7 +51,7 @@ class User {
     }
 
     private function validatePasswordUnsafe(string $password): bool {
-        return \strlen($password) >= 8;
+        return strlen($password) >= 8;
     }
 
     private function validatePasswordBasic(string $password): bool {
